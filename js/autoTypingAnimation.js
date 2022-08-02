b571cfc61369e90f2732f46778f813ec8d1ceed1
Aaka18sh <82805453+Aaka18sh@users.noreply.github.com>
2022-08-02 11:39:08 +0530

    new

---
+++ js/autoTypingAnimation.js
@@ -0,0 +1,9 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+      strings: [
+        "<span>Hi, I am Aakash Singh Rajput</span> "
+      ],
+      typeSpeed: 7,
+      contentType: 'html'
+    });
+  }, 100);
